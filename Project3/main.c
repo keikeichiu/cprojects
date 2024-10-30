3dcba1acfda437c7118a9d6911bd9e5fd43f4bc9
keikeichiu <zhaoyongqi0911@proton.me>
2024-10-30 16:44:46 +0800

    Initial Commit

---
+++ Project3/main.c
@@ -0,0 +1,23 @@
+//
+//  main.c
+//  Project3
+//
+//  Created by tom on 30/10/2024.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    printf("Great!!!!\n");
+    printf("Great!!!!\n");
+
+    printf("Great!!!!\n");
+    printf("Great!!!!\n");
+    printf("Great!!!!\n");
+    printf("Great!!!!\n");
+
+
+    return 0;
+}
